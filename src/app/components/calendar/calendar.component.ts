51b5bd50959129021f0beaf686b561182b488013
Fernando Alvarez <feralvarz@gmail.com>
2021-03-30 12:06:36 +0100

    Calendar component with month navigation

---
+++ src/app/components/calendar/calendar.component.ts
@@ -0,0 +1,107 @@
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import * as moment from 'moment';
+
+interface IMonth {
+  value?: moment.Moment;
+  events?: any[];
+  days: number[];
+}
+
+interface ICalendar {
+  previousMonth: IMonth;
+  currentMonth: IMonth;
+  nextMonth: IMonth;
+}
+
+@Component({
+  selector: 'app-calendar',
+  templateUrl: './calendar.component.html',
+  styleUrls: ['./calendar.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class CalendarComponent implements OnInit {
+  calendar: ICalendar = {
+    previousMonth: { days: [] },
+    currentMonth: { days: [] },
+    nextMonth: { days: [] },
+  };
+
+  daysOfWeek: string[] = [
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday',
+  ];
+
+  ngOnInit(): void {
+    const currentMonth = new Date();
+    this.fillCalendar(currentMonth);
+  }
+
+  fillCalendar(date: Date) {
+    this.calendar.previousMonth.days = [];
+    this.calendar.nextMonth.days = [];
+
+    this.calendar.currentMonth.value = moment(date);
+    const from = moment(date).startOf('month');
+    const to = from.clone().endOf('month');
+    const dayCount = to.clone().add(1, 'day').diff(from, 'days');
+
+    // Creates an array from [1,...,n] days
+    this.calendar.currentMonth.days = Array.from(
+      Array(dayCount),
+      (x, i) => i + 1
+    );
+
+    this.fillPreviousDays(from);
+    this.fillNextDays(to);
+  }
+
+  /**
+   * Updates the calendar with next month
+   */
+  prevMonth(): void {
+    this.fillCalendar(
+      this.calendar.currentMonth.value.subtract(1, 'month').toDate()
+    );
+  }
+
+  /**
+   * Updates the calendar with previous month
+   */
+  nextMonth(): void {
+    this.fillCalendar(
+      this.calendar.currentMonth.value.add(1, 'month').toDate()
+    );
+  }
+
+  /**
+   * Fills empty spaces in calendar before current month
+   * @param from first day of current month used to calculate days
+   */
+  private fillPreviousDays(from: moment.Moment): void {
+    const fromDayIndex = from.weekday();
+    if (fromDayIndex > 0) {
+      const lastDayPrevMonth = +from.clone().subtract(1, 'day').format('DD');
+      for (let i = 0; i < fromDayIndex; i++) {
+        this.calendar.previousMonth.days.unshift(lastDayPrevMonth - i);
+      }
+    }
+  }
+
+  /**
+   * Fills empty spaces in calendar after current month
+   * @param to Last day of current month used to calculate days
+   */
+  private fillNextDays(to: moment.Moment): void {
+    const toDayIndex = to.isoWeekday();
+    if (toDayIndex < 6) {
+      for (let i = 1; i <= 6 - toDayIndex; i++) {
+        this.calendar.nextMonth.days.push(i);
+      }
+    }
+  }
+}
